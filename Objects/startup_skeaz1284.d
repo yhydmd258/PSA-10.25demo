73592d5dee3ad37b95bde91cf3f6c687f5d51bbc
haiyun yan <haiyun_yan@tianma.cn>
2020-05-29 14:51:16 +0800

    背光改成60

--- Objects/startup_skeaz1284.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_skeaz1284.o: Device\Startup\startup_SKEAZ1284.s
--- Objects/system_skeaz1284.d
+++
@@ -1,7 +0,0 @@
-.\objects\system_skeaz1284.o: Device\sources\system_SKEAZ1284.c
-.\objects\system_skeaz1284.o: D:\MDK\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_skeaz1284.o: .\Device\headers\SKEAZ1284.h
-.\objects\system_skeaz1284.o: .\CMSIS\core_cm0plus.h
-.\objects\system_skeaz1284.o: .\CMSIS\core_cmInstr.h
-.\objects\system_skeaz1284.o: .\CMSIS\core_cmFunc.h
-.\objects\system_skeaz1284.o: .\Device\headers\system_SKEAZ1284.h
